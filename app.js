12ce9c5490f54d0f01731e6f930e45143de16784
unknown <jswon7942@naver.com>
2021-10-14 14:53:13 +0900

    heroku hello world test

--- app.js
+++ app.js
@@ -49,14 +49,23 @@
 // app.use(cors(corsOptions));
 
 
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
 
-router.get('/', function (req, res, next) {
-  res.status(200).json({
-    message: "sopt 서버 여러분 안녕하세요~,sopt-Media에 좋아요와 댓글 부탁드립니다!!(꾸벅)",
-  })
-});
+// router.get('/', function (req, res, next) {
+//   res.status(200).json({
+//     message: "sopt 서버 여러분 안녕하세요~,sopt-Media에 좋아요와 댓글 부탁드립니다!!(꾸벅)",
+//   })
+// });
 
-module.exports = router;
+// module.exports = router;
+
+
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`err ${port}`));
